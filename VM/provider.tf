380be345c1526f6e035587bcd362adb7b5565880
shubhamagrawal17 <subhek.agrawal@gmail.com>
2024-02-25 14:37:55 +0530

    Update provider.tf

--- VM/provider.tf
+++ VM/provider.tf
@@ -1,8 +1,8 @@
 provider "azurerm" {
   features {}
   skip_provider_registration = true
-  client_id                  = "638a1b32-6645-4273-84e2-7f3cefabfd97"
-  client_secret              = "I368Q~qrGYvznvv4R65q3Plc03PTndGfw8L6IaW6"
-  tenant_id                  = "d8558947-5dc9-4df0-8614-312e6f7f4492"
-  subscription_id            = "40dcd18b-2889-4b3c-9a26-69143ed8b061"
+  client_id                  = "your client id"
+  client_secret              = "your client secret"
+  tenant_id                  = "your tenant id"
+  subscription_id            = "your subscription_id"
 }
