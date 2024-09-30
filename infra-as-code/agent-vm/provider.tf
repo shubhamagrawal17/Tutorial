4b59dab073bf349f2fe0843ec9d16fbf471697d8
shubhamagrawal17 <subhek.agrawal@gmail.com>
2024-10-01 00:46:34 +0530

    Update provider.tf

--- infra-as-code/agent-vm/provider.tf
+++ infra-as-code/agent-vm/provider.tf
@@ -1,6 +1,7 @@
+# Configure the Microsoft Azure Provider
 provider "azurerm" {
+  resource_provider_registrations = "none" # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
   features {}
-  skip_provider_registration = true
 }
 terraform {
   backend "azurerm" {
